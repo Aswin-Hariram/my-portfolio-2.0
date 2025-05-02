1ec4df16822a96af40a4b3fb9afb01b600ec7360
Aswin-Hariram <aswincseskct@gmail.com>
2025-05-02 16:43:33 +0530

    chatbot update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // This exposes the server to your network
+    port: 3000  // Optional: specify a port
+  },
   plugins: [react()],
 })
