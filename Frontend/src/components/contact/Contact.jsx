57dbdca71ba06bd98ba13d96d860e1443a380715
aswin-hariram <aswincseskct@gmail.com>
2026-03-16 00:28:47 +0530

    leetcode update

--- Frontend/src/components/contact/Contact.jsx
+++ Frontend/src/components/contact/Contact.jsx
@@ -25,8 +25,8 @@ const Contact = () => {
   // Social media links data for reusability
   const socialLinks = [
     {
-      name: "Reddit",
-      url: "https://www.reddit.com/user/Fun_Paper7159/",
+      name: "LeetCode",
+      url: "https://leetcode.com/u/Q2kEKh00sY/",
       icon: (
         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M12 1C5.925 1 1 5.925 1 12C1 18.075 5.925 23 12 23C18.075 23 23 18.075 23 12C23 5.925 18.075 1 12 1Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
